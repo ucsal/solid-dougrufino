405ff73ed146481d7bb8622be0782a0351032c66
Douglas Rufino <douglasvitorio367@gmail.com>
2026-03-29 22:28:46 -0300

    Refatoração SRP
    Criação de construtor para inicialização dos atributos
    Remoção dos métodos set para evitar mudança dos dados
    Melhoria no encapsulamento da classe
    Garantia de que o objeto já é criado em estado válido

--- src/main/java/br/com/ucsal/olimpiadas/Participante.java
+++ src/main/java/br/com/ucsal/olimpiadas/Participante.java
@@ -5,27 +5,24 @@ public class Participante {
 	private String nome;
 	private String email;
 
-	public long getId() {
-		return id;
+	public Participante(long id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+	public long getId() {
+		return id;
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public String getEmail() {
 		return email;
 	}
+} 
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-}
+
+
